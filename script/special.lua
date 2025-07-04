02f0a6ba2359a1aef4a2e66177c603d4e96822c8
jwyxym <jwyxym@126.com>
2025-07-04 15:50:29 +0800

    修改special.lua调用方法

--- script/special.lua
+++ script/special.lua
@@ -1,18 +1,17 @@
-function Auxiliary.PreloadUds()
-    function require(str)
-        dofile(str..".lua")
-        --[[local len = string.len(str)
-        local result=''
-        for i=len,1,-1 do
-            local temp=""
-            temp=string.sub(str,i,i)
-            if temp=="/" then
-                result=result..".lua"
-                Duel.LoadScript(result)
-                return
-            end
-            result=temp..result
+require = function (str)
+    dofile(str..".lua")
+end
+dofile = function (str)
+    local len = string.len(str)
+    local result=''
+    for i=len,1,-1 do
+        local temp=""
+        temp=string.sub(str,i,i)
+        if temp=="/" then
+            result=result..".lua"
+            Duel.LoadScript(result)
+            return
         end
-        ]]--
+        result=temp..result
     end
 end
